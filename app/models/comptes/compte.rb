fa27b017ee9cc00f5dce4847a293130df64e1d2c
Kineolyan <kineolyan@gmail.com>
2013-12-15 00:12:21 +0100

    Implémente la modification du compte.

--- app/models/comptes/compte.rb
+++ app/models/comptes/compte.rb
@@ -1,7 +1,11 @@
 class Comptes::Compte < ActiveRecord::Base
-  attr_readonly :solde
   has_many :transactions
 
   validates :nom, presence: true, uniqueness: true
   validates :solde, presence: true, numericality: { only_integer: true }
+
+  public
+  def solde_formatte
+    ApplicationHelper::format_amount solde
+  end
 end
