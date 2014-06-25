a41e3ab6a94590d0297741309399946963d2072e
Kineolyan <kineolyan@gmail.com>
2014-06-25 21:46:49 +0200

    Ajoute une méthode pour créer des glyphicons.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,4 +14,16 @@ module ApplicationHelper
     ! /^-?\d+(\.\d+)?$/.match(value.to_s).nil?
   end
 
+  # Create a Bootstrap glyphicon in a tag
+  # Options can contains:
+  #   :btn to create a button. Ex { btn: "danger" }
+  #   :text to add text after the glyphicon. Ex { text: "Edit" }
+  # Returns the html for the glyphicon
+  def self.glyphicon name, options = {}
+    additional_classes=""
+    additional_classes << "btn btn-#{options[:btn]}" if options.key? :btn
+
+    "<i class=\"glyphicon glyphicon-#{name} #{additional_classes}\">#{options[:text]}</i>".html_safe
+  end
+
 end
