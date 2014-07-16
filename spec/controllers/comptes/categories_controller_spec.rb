afc117a9d31259331fbe4ffff3d51aa96eb4f933
Kineolyan <kineolyan@gmail.com>
2014-07-16 21:17:26 +0200

    Remove tests on views and controllers (to replace with capybara tests).

--- spec/controllers/comptes/categories_controller_spec.rb
+++ spec/controllers/comptes/categories_controller_spec.rb
@@ -20,140 +20,140 @@ require 'rails_helper'
 
 RSpec.describe Comptes::CategoriesController, :type => :controller do
 
-  # This should return the minimal set of attributes required to create a valid
-  # Comptes::Categorie. As you add validations to Comptes::Categorie, be sure to
-  # adjust the attributes here as well.
-  let(:valid_attributes) {
-    skip("Add a hash of attributes valid for your model")
-  }
+  # # This should return the minimal set of attributes required to create a valid
+  # # Comptes::Categorie. As you add validations to Comptes::Categorie, be sure to
+  # # adjust the attributes here as well.
+  # let(:valid_attributes) {
+  #   skip("Add a hash of attributes valid for your model")
+  # }
 
-  let(:invalid_attributes) {
-    skip("Add a hash of attributes invalid for your model")
-  }
+  # let(:invalid_attributes) {
+  #   skip("Add a hash of attributes invalid for your model")
+  # }
 
-  # This should return the minimal set of values that should be in the session
-  # in order to pass any filters (e.g. authentication) defined in
-  # Comptes::CategoriesController. Be sure to keep this updated too.
-  let(:valid_session) { {} }
+  # # This should return the minimal set of values that should be in the session
+  # # in order to pass any filters (e.g. authentication) defined in
+  # # Comptes::CategoriesController. Be sure to keep this updated too.
+  # let(:valid_session) { {} }
 
-  describe "GET index" do
-    it "assigns all comptes_categories as @comptes_categories" do
-      categorie = Comptes::Categorie.create! valid_attributes
-      get :index, {}, valid_session
-      expect(assigns(:comptes_categories)).to eq([categorie])
-    end
-  end
+  # describe "GET index" do
+  #   it "assigns all comptes_categories as @comptes_categories" do
+  #     categorie = Comptes::Categorie.create! valid_attributes
+  #     get :index, {}, valid_session
+  #     expect(assigns(:comptes_categories)).to eq([categorie])
+  #   end
+  # end
 
-  describe "GET show" do
-    it "assigns the requested comptes_categorie as @comptes_categorie" do
-      categorie = Comptes::Categorie.create! valid_attributes
-      get :show, {:id => categorie.to_param}, valid_session
-      expect(assigns(:comptes_categorie)).to eq(categorie)
-    end
-  end
+  # describe "GET show" do
+  #   it "assigns the requested comptes_categorie as @comptes_categorie" do
+  #     categorie = Comptes::Categorie.create! valid_attributes
+  #     get :show, {:id => categorie.to_param}, valid_session
+  #     expect(assigns(:comptes_categorie)).to eq(categorie)
+  #   end
+  # end
 
-  describe "GET new" do
-    it "assigns a new comptes_categorie as @comptes_categorie" do
-      get :new, {}, valid_session
-      expect(assigns(:comptes_categorie)).to be_a_new(Comptes::Categorie)
-    end
-  end
+  # describe "GET new" do
+  #   it "assigns a new comptes_categorie as @comptes_categorie" do
+  #     get :new, {}, valid_session
+  #     expect(assigns(:comptes_categorie)).to be_a_new(Comptes::Categorie)
+  #   end
+  # end
 
-  describe "GET edit" do
-    it "assigns the requested comptes_categorie as @comptes_categorie" do
-      categorie = Comptes::Categorie.create! valid_attributes
-      get :edit, {:id => categorie.to_param}, valid_session
-      expect(assigns(:comptes_categorie)).to eq(categorie)
-    end
-  end
+  # describe "GET edit" do
+  #   it "assigns the requested comptes_categorie as @comptes_categorie" do
+  #     categorie = Comptes::Categorie.create! valid_attributes
+  #     get :edit, {:id => categorie.to_param}, valid_session
+  #     expect(assigns(:comptes_categorie)).to eq(categorie)
+  #   end
+  # end
 
-  describe "POST create" do
-    describe "with valid params" do
-      it "creates a new Comptes::Categorie" do
-        expect {
-          post :create, {:comptes_categorie => valid_attributes}, valid_session
-        }.to change(Comptes::Categorie, :count).by(1)
-      end
+  # describe "POST create" do
+  #   describe "with valid params" do
+  #     it "creates a new Comptes::Categorie" do
+  #       expect {
+  #         post :create, {:comptes_categorie => valid_attributes}, valid_session
+  #       }.to change(Comptes::Categorie, :count).by(1)
+  #     end
 
-      it "assigns a newly created comptes_categorie as @comptes_categorie" do
-        post :create, {:comptes_categorie => valid_attributes}, valid_session
-        expect(assigns(:comptes_categorie)).to be_a(Comptes::Categorie)
-        expect(assigns(:comptes_categorie)).to be_persisted
-      end
+  #     it "assigns a newly created comptes_categorie as @comptes_categorie" do
+  #       post :create, {:comptes_categorie => valid_attributes}, valid_session
+  #       expect(assigns(:comptes_categorie)).to be_a(Comptes::Categorie)
+  #       expect(assigns(:comptes_categorie)).to be_persisted
+  #     end
 
-      it "redirects to the created comptes_categorie" do
-        post :create, {:comptes_categorie => valid_attributes}, valid_session
-        expect(response).to redirect_to(Comptes::Categorie.last)
-      end
-    end
+  #     it "redirects to the created comptes_categorie" do
+  #       post :create, {:comptes_categorie => valid_attributes}, valid_session
+  #       expect(response).to redirect_to(Comptes::Categorie.last)
+  #     end
+  #   end
 
-    describe "with invalid params" do
-      it "assigns a newly created but unsaved comptes_categorie as @comptes_categorie" do
-        post :create, {:comptes_categorie => invalid_attributes}, valid_session
-        expect(assigns(:comptes_categorie)).to be_a_new(Comptes::Categorie)
-      end
+  #   describe "with invalid params" do
+  #     it "assigns a newly created but unsaved comptes_categorie as @comptes_categorie" do
+  #       post :create, {:comptes_categorie => invalid_attributes}, valid_session
+  #       expect(assigns(:comptes_categorie)).to be_a_new(Comptes::Categorie)
+  #     end
 
-      it "re-renders the 'new' template" do
-        post :create, {:comptes_categorie => invalid_attributes}, valid_session
-        expect(response).to render_template("new")
-      end
-    end
-  end
+  #     it "re-renders the 'new' template" do
+  #       post :create, {:comptes_categorie => invalid_attributes}, valid_session
+  #       expect(response).to render_template("new")
+  #     end
+  #   end
+  # end
 
-  describe "PUT update" do
-    describe "with valid params" do
-      let(:new_attributes) {
-        skip("Add a hash of attributes valid for your model")
-      }
+  # describe "PUT update" do
+  #   describe "with valid params" do
+  #     let(:new_attributes) {
+  #       skip("Add a hash of attributes valid for your model")
+  #     }
 
-      it "updates the requested comptes_categorie" do
-        categorie = Comptes::Categorie.create! valid_attributes
-        put :update, {:id => categorie.to_param, :comptes_categorie => new_attributes}, valid_session
-        categorie.reload
-        skip("Add assertions for updated state")
-      end
+  #     it "updates the requested comptes_categorie" do
+  #       categorie = Comptes::Categorie.create! valid_attributes
+  #       put :update, {:id => categorie.to_param, :comptes_categorie => new_attributes}, valid_session
+  #       categorie.reload
+  #       skip("Add assertions for updated state")
+  #     end
 
-      it "assigns the requested comptes_categorie as @comptes_categorie" do
-        categorie = Comptes::Categorie.create! valid_attributes
-        put :update, {:id => categorie.to_param, :comptes_categorie => valid_attributes}, valid_session
-        expect(assigns(:comptes_categorie)).to eq(categorie)
-      end
+  #     it "assigns the requested comptes_categorie as @comptes_categorie" do
+  #       categorie = Comptes::Categorie.create! valid_attributes
+  #       put :update, {:id => categorie.to_param, :comptes_categorie => valid_attributes}, valid_session
+  #       expect(assigns(:comptes_categorie)).to eq(categorie)
+  #     end
 
-      it "redirects to the comptes_categorie" do
-        categorie = Comptes::Categorie.create! valid_attributes
-        put :update, {:id => categorie.to_param, :comptes_categorie => valid_attributes}, valid_session
-        expect(response).to redirect_to(categorie)
-      end
-    end
+  #     it "redirects to the comptes_categorie" do
+  #       categorie = Comptes::Categorie.create! valid_attributes
+  #       put :update, {:id => categorie.to_param, :comptes_categorie => valid_attributes}, valid_session
+  #       expect(response).to redirect_to(categorie)
+  #     end
+  #   end
 
-    describe "with invalid params" do
-      it "assigns the comptes_categorie as @comptes_categorie" do
-        categorie = Comptes::Categorie.create! valid_attributes
-        put :update, {:id => categorie.to_param, :comptes_categorie => invalid_attributes}, valid_session
-        expect(assigns(:comptes_categorie)).to eq(categorie)
-      end
+  #   describe "with invalid params" do
+  #     it "assigns the comptes_categorie as @comptes_categorie" do
+  #       categorie = Comptes::Categorie.create! valid_attributes
+  #       put :update, {:id => categorie.to_param, :comptes_categorie => invalid_attributes}, valid_session
+  #       expect(assigns(:comptes_categorie)).to eq(categorie)
+  #     end
 
-      it "re-renders the 'edit' template" do
-        categorie = Comptes::Categorie.create! valid_attributes
-        put :update, {:id => categorie.to_param, :comptes_categorie => invalid_attributes}, valid_session
-        expect(response).to render_template("edit")
-      end
-    end
-  end
+  #     it "re-renders the 'edit' template" do
+  #       categorie = Comptes::Categorie.create! valid_attributes
+  #       put :update, {:id => categorie.to_param, :comptes_categorie => invalid_attributes}, valid_session
+  #       expect(response).to render_template("edit")
+  #     end
+  #   end
+  # end
 
-  describe "DELETE destroy" do
-    it "destroys the requested comptes_categorie" do
-      categorie = Comptes::Categorie.create! valid_attributes
-      expect {
-        delete :destroy, {:id => categorie.to_param}, valid_session
-      }.to change(Comptes::Categorie, :count).by(-1)
-    end
+  # describe "DELETE destroy" do
+  #   it "destroys the requested comptes_categorie" do
+  #     categorie = Comptes::Categorie.create! valid_attributes
+  #     expect {
+  #       delete :destroy, {:id => categorie.to_param}, valid_session
+  #     }.to change(Comptes::Categorie, :count).by(-1)
+  #   end
 
-    it "redirects to the comptes_categories list" do
-      categorie = Comptes::Categorie.create! valid_attributes
-      delete :destroy, {:id => categorie.to_param}, valid_session
-      expect(response).to redirect_to(comptes_categories_url)
-    end
-  end
+  #   it "redirects to the comptes_categories list" do
+  #     categorie = Comptes::Categorie.create! valid_attributes
+  #     delete :destroy, {:id => categorie.to_param}, valid_session
+  #     expect(response).to redirect_to(comptes_categories_url)
+  #   end
+  # end
 
 end
