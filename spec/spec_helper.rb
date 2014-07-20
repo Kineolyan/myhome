03451d0609656878fc309b539acfebb4e97fb8b3
Kineolyan <kineolyan@gmail.com>
2014-07-20 11:30:54 +0200

    Improve rspec configuration.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,17 +14,65 @@ end
 
 def configure_rspec
   RSpec.configure do |config|
-    # == Mock Framework
-    #
-    # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
-    #
-    # config.mock with :mocha
-    # config.mock with :flexmock
-    # config.mock with :rr
-    config.mock_with :rspec
+    # Running tag focus or everything if not defined
+    config.filter_run :focus
+    config.run_all_when_everything_filtered = true
+
+    # Configuration for run on single file
+    if config.files_to_run.one?
+      config.default_formatter = 'doc'
+    end
+
+    # Give the 5 slowest tests
+    config.profile_examples = 5
+
+    # Randomization options
+    # Run specs in random order to surface order dependencies. If you find an
+    # order dependency and want to debug it, you can fix the order by providing
+    # the seed, which is printed after each run.
+    #     --seed 1234
+    config.order = :random
+
+    # Seed global randomization in this process using the `--seed` CLI option.
+    # Setting this allows you to use `--seed` to deterministically reproduce
+    # test failures related to randomization by passing the same `--seed` value
+    # as the one that triggered the failure.
+    Kernel.srand config.seed
+
+    config.expect_with :rspec do |expectations|
+      # Enable only the newer, non-monkey-patching expect syntax.
+      # For more details, see:
+      #   - http://myronmars.to/n/dev-blog/2012/06/rspecs-new-expectation-syntax
+      expectations.syntax = :expect
+    end
+
+    # rspec-mocks config goes here. You can use an alternate test double
+    # library (such as bogus or mocha) by changing the `mock_with` option here.
+    config.mock_with :rspec do |mocks|
+      mocks.syntax = :expect
+
+      # Prevents you from mocking or stubbing a method that does not exist on
+      # a real object. This is generally recommended.
+      mocks.verify_partial_doubles = true
+    end
 
     # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
     config.fixture_path = "#{::Rails.root}/spec/fixtures"
+
+    # RSpec Rails can automatically mix in different behaviours to your tests
+    # based on their file location, for example enabling you to call `get` and
+    # `post` in specs under `spec/controllers`.
+    #
+    # You can disable this behaviour by removing the line below, and instead
+    # explicitly tag your specs with their type, e.g.:
+    #
+    #     RSpec.describe UsersController, :type => :controller do
+    #       # ...
+    #     end
+    #
+    # The different available types are documented in the features, such as in
+    # https://relishapp.com/rspec/rspec-rails/docs
+    config.infer_spec_type_from_file_location!
 
     # If you're not using ActiveRecord, or you'd prefer not to run each of your
     # examples within a transaction, remove the following line or assign false
@@ -35,12 +83,6 @@ def configure_rspec
     # automatically. This will be the default behavior in future versions of
     # rspec-rails.
     config.infer_base_class_for_anonymous_controllers = false
-
-    # Run specs in random order to surface order dependencies. If you find an
-    # order dependency and want to debug it, you can fix the order by providing
-    # the seed, which is printed after each run.
-    #     --seed 1234
-    config.order = "random"
   end
 end
 
