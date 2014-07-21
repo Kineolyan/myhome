54fe8111b39b85a67dce069d3260faaad1fc42f4
Kineolyan <kineolyan@gmail.com>
2014-07-21 23:47:19 +0200

    Improve configuration of rspec.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,7 +5,6 @@ def load_env
   ENV["RAILS_ENV"] ||= 'test'
   require File.expand_path("../../config/environment", __FILE__)
   require 'rspec/rails'
-  require_relative "custom_matchers.rb"
 
   # Requires supporting ruby files with custom matchers and macros, etc,
   # in spec/support/ and its subdirectories.
