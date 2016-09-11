42635b31c14cfc1accb04cbef2682bba025693d1
Kineolyan <kineolyan@gmail.com>
2016-09-11 18:17:22 +0200

    Reviewed with stamps: working validation.

--- src/comptes/validations/AccountBalance.js
+++ src/comptes/validations/AccountBalance.js
@@ -1,101 +1,209 @@
 import React from 'react';
 import _ from 'lodash';
+import reactStamp from 'react-stamp';
 
 import {Type} from '../../transactions/models';
 
-class AccountBalance extends React.Component {
-	constructor(props) {
-		super(props);
+function nextDay(date) {
+	return new Date(date).setHours(23, 59, 59, 999) + 1;
+}
 
-		this.state = {
-			balance: null,
-			validation: null
-		};
-		this.streams = {};
-	}
-
-	get validationFeed() {
+const WithHorizons = {
+	contextTypes: {
+		horizons: React.PropTypes.object
+	},
+	getValidationFeed() {
 		return this.context.horizons.validations;
-	}
-
-	get transactionFeed() {
+	},
+	getTransactionFeed() {
 		return this.context.horizons.transactions;
 	}
+}
 
-	getBalanceDate() {
-		if (_.isNumber(this.props.date)) {
-			return this.props.date;
-		} else if (_.isString(this.props.date)) {
-			return new Date(this.props.date).getTime();
-		} else { // Must be a date
-			return this.props.date.getTime();
+const UnvalidatedTransactions = {
+	fetchLatestValidation(account) {
+		return this.getValidationFeed()
+			.findAll({account: account})
+			.order('validatedAt', 'descending')
+			.limit(1);
+	},
+	fetchUnvalidatedTransactions(account, validation, date) {
+		let stream = this.getTransactionFeed().findAll({account});
+		if (validation) {
+			stream = stream.above({date: nextDay(validation.validationDate)}, 'closed');
 		}
+		if (date) {
+			stream = stream.below({date: nextDay(date)});
+		}
+		return stream;
 	}
+};
 
-	componentDidMount() {
-		this.fetchValidation(this.props.account);
-	}
-
+const WithStreams = {
+	init(props, {instance}) {
+		instance.streams = {};
+	},
+	setStream(name, stream) {
+		if (name in this.streams) {
+			this.streams[name].unsubscribe();
+		}
+		this.streams[name] = stream;
+	},
 	componentWillUnmount() {
 		_.forEach(this.streams, stream => stream.unsubscribe());
 		this.streams = {};
 	}
-
-	componentWillReceiveProps(nextProps) {
-		if (this.props.account !== nextProps.account) {
-			this.setState({
-				balance: null,
-				validation: null
-			});
-			this.fetchValidation(nextProps.account);
-		}
-	}
-
-	fetchValidation(account) {
-		this.streams.validation = this.validationFeed
-			.findAll({account: account})
-			.order('validatedAt', 'descending')
-			.limit(1)
-			.fetch().defaultIfEmpty()
-			.subscribe(
-				([validation]) => this.setValidation(validation),
-				error => console.error('[Failure] Cannot retrieve last validation', error)
-			);
-	}
-
-	setValidation(account, validation) {
-		this.setState({validation});
-		let stream = this.transactionFeed
-			.findAll({account: this.props.account});
-		let lastBalance = 0;
-		if (validation) {
-			stream = stream.above({date: validation.validationDate}, 'closed');
-			lastBalance = validation.balance;
-		}
-		if (this.props.date) {
-			stream = stream.below({date: this.getBalanceDate()});
-		}
-		this.streams.balance = stream.watch()
-			.filter(transaction => transaction.type !== Type.MONNAIE)
-			.subscribe(
-				transactions => {
-					const balance = lastBalance + _.sumBy(transactions, 'amount');
-					this.setState({balance});
-				},
-				error => console.log('[Failure] Cannot compute balance', error)
-			);
-	}
-
-	render() {
-		if (this.state.balance === null) {
-			return <div>Computing ...</div>;
-		}
-
-		return <div className="account-balance">
-			Solde du compte: {this.state.balance.toFixed(2)} €
-		</div>;
-	}
 }
+
+const AccountBalance = reactStamp(React)
+	.compose(WithHorizons, UnvalidatedTransactions, WithStreams)
+	.compose({
+		propTypes: {
+			account: React.PropTypes.string.isRequired,
+			date: React.PropTypes.number
+		},
+		state: {
+			balance: null,
+			validation: null
+		},
+		getBalanceDate() {
+			return this.props.date;
+		},
+		componentDidMount() {
+			this.fetchValidation(this.props.account);
+		},
+		componentWillReceiveProps(nextProps) {
+			if (this.props.account !== nextProps.account) {
+				this.setState({
+					balance: null,
+					validation: null
+				});
+				this.fetchValidation(nextProps.account);
+			}
+		},
+		fetchValidation(account) {
+			const stream = this.fetchLatestValidation(account)
+				.fetch().defaultIfEmpty()
+				.subscribe(
+					([validation]) => this.setValidation(validation),
+					error => console.error('[Failure] Cannot retrieve last validation', error)
+				);
+			this.setStream('validation', stream);
+		},
+		setValidation(validation) {
+			this.setState({validation});
+			const date = this.props.date ? this.getBalanceDate() : null;
+			const lastBalance = validation ? validation.balance : 0;
+			const stream = this.fetchUnvalidatedTransactions(this.props.account, validation, date)
+				.watch()
+				.filter(transaction => transaction.type !== Type.MONNAIE)
+				.subscribe(
+					transactions => {
+						const balance = lastBalance + _.sumBy(transactions, 'amount');
+						this.setState({balance});
+					},
+					error => console.log('[Failure] Cannot compute balance', error)
+				);
+			this.setStream('transactions', stream);
+		},
+		render() {
+			if (this.state.balance === null) {
+				return <div>Computing ...</div>;
+			}
+
+			return <div className="account-balance">
+				Solde du compte: {this.state.balance.toFixed(2)} €
+			</div>;
+		}
+	});
+// class AccountBalance extends React.Component {
+// 	constructor(props) {
+// 		super(props);
+
+// 		this.state = {
+// 			balance: null,
+// 			validation: null
+// 		};
+// 		this.streams = {};
+// 	}
+
+// 	get validationFeed() {
+// 		return this.context.horizons.validations;
+// 	}
+
+// 	get transactionFeed() {
+// 		return this.context.horizons.transactions;
+// 	}
+
+// 	getBalanceDate() {
+// 		if (_.isNumber(this.props.date)) {
+// 			return this.props.date;
+// 		} else if (_.isString(this.props.date)) {
+// 			return new Date(this.props.date).getTime();
+// 		} else { // Must be a date
+// 			return this.props.date.getTime();
+// 		}
+// 	}
+
+// 	componentDidMount() {
+// 		this.fetchValidation(this.props.account);
+// 	}
+
+// 	componentWillReceiveProps(nextProps) {
+// 		if (this.props.account !== nextProps.account) {
+// 			this.setState({
+// 				balance: null,
+// 				validation: null
+// 			});
+// 			this.fetchValidation(nextProps.account);
+// 		}
+// 	}
+
+// 	fetchValidation(account) {
+// 		this.streams.validation = this.validationFeed
+// 			.findAll({account: account})
+// 			.order('validatedAt', 'descending')
+// 			.limit(1)
+// 			.fetch().defaultIfEmpty()
+// 			.subscribe(
+// 				([validation]) => this.setValidation(validation),
+// 				error => console.error('[Failure] Cannot retrieve last validation', error)
+// 			);
+// 	}
+
+// 	setValidation(account, validation) {
+// 		this.setState({validation});
+// 		let stream = this.transactionFeed
+// 			.findAll({account: this.props.account});
+// 		let lastBalance = 0;
+// 		if (validation) {
+// 			stream = stream.above({date: validation.validationDate}, 'closed');
+// 			lastBalance = validation.balance;
+// 		}
+// 		if (this.props.date) {
+// 			stream = stream.below({date: this.getBalanceDate()});
+// 		}
+// 		this.streams.balance = stream.watch()
+// 			.filter(transaction => transaction.type !== Type.MONNAIE)
+// 			.subscribe(
+// 				transactions => {
+// 					const balance = lastBalance + _.sumBy(transactions, 'amount');
+// 					this.setState({balance});
+// 				},
+// 				error => console.log('[Failure] Cannot compute balance', error)
+// 			);
+// 	}
+
+// 	render() {
+// 		if (this.state.balance === null) {
+// 			return <div>Computing ...</div>;
+// 		}
+
+// 		return <div className="account-balance">
+// 			Solde du compte: {this.state.balance.toFixed(2)} €
+// 		</div>;
+// 	}
+// }
 
 AccountBalance.propTypes = {
 	account: React.PropTypes.string.isRequired,
@@ -114,3 +222,8 @@ AccountBalance.contextTypes = {
 };
 
 export default AccountBalance;
+export {
+	WithHorizons,
+	UnvalidatedTransactions,
+	WithStreams
+};
--- src/comptes/validations/AccountValidator.js
+++ src/comptes/validations/AccountValidator.js
@@ -1,138 +1,163 @@
 import React from 'react';
 import _ from 'lodash';
+import reactStamp from 'react-stamp';
 
 import TextField from 'material-ui/TextField';
 import RaisedButton from 'material-ui/RaisedButton';
 import DatePicker from 'material-ui/DatePicker';
+import FloatingActionButton from 'material-ui/FloatingActionButton';
+import DeleteIcon from 'material-ui/svg-icons/action/delete';
 
 import AccountPicker from '../AccountPicker';
-import AccountBalance from './AccountBalance';
+import AccountBalance, {WithHorizons, UnvalidatedTransactions, WithStreams} from './AccountBalance';
 import TransactionsView from '../../transactions/TransactionsView';
 
 const TODAY = new Date();
 
-class AccountValidator extends React.Component {
-	constructor(props) {
-		super(props);
-
-		this.state = {
+const AccountValidator = reactStamp(React)
+	.compose(WithHorizons, UnvalidatedTransactions, WithStreams)
+	.compose({
+		state: {
 			account: null,
 			lastValidation: null,
 			balance: null,
 			validationDate: TODAY
-		};
-
-		this.cbks = {
-			setAccount: this.setAccount.bind(this),
-			setBalance: this.setBalance.bind(this),
-			setValidationDate: this.setValidationDate.bind(this),
-			validate: this.validate.bind(this)
-		}
-	}
-
-	get validationFeed() {
-		return this.context.horizons.validations;
-	}
-
-	get transactionFeed() {
-		return this.context.horizons.transactions;
-	}
-
-	setAccount(account) {
-		this.setState({account});
-		this.transactionFeed.findAll({account})
-			.order('date', 'descending')
-			.watch()
-			.subscribe(
-				transactions => this.setState({transactions}),
-				error => console.error('[Failure]', 'Fetch transactions', error)
-			)
-	}
-
-	setBalance(event, value) {
-		this.setState({balance: parseFloat(value)});
-	}
-
-	setValidationDate(event, date) {
-		this.setState({validationDate: date});
-	}
-
-	validate() {
-		const validation = {
-			account: this.state.account,
-			balance: this.state.balance,
-			validatedAt: Date.now(),
-			validationDate: this.state.validationDate.getTime()
-		};
-		this.validationFeed.store(validation).subscribe(
-			() => console.log('Account balanced validated'),
-			error => console.error('[Failure] Account validation', error)
-		);
-	}
-
-	renderTransactions() {
-		if (!_.isEmpty(this.state.transactions)) {
-			// Group transactions by date
-			const lastWeek = new Date();
-			lastWeek.setDate(TODAY.getDate() - 7);
-			lastWeek.setHours(0);
-			lastWeek.setMinutes(0);
-			lastWeek.setSeconds(0);
-			lastWeek.setMilliseconds(0);
-
-			const groups = _(this.state.transactions)
-				.filter(transaction => transaction.date >= lastWeek)
-				.groupBy(transaction => new Date(transaction.date).toLocaleDateString())
-				.value();
-			const oldTransactions = _.filter(this.state.transactions, transaction => transaction.date < lastWeek);
-			if (!_.isEmpty(oldTransactions)) {
-				groups.Before = oldTransactions;
+		},
+		init(props, {instance}) {
+			instance.cbks = {
+				setAccount: instance.setAccount.bind(instance),
+				setBalance: instance.setBalance.bind(instance),
+				setValidationDate: instance.setValidationDate.bind(instance),
+				validate: instance.validate.bind(instance),
+				deleteLastValidation: instance.deleteLastValidation.bind(instance)
+			};
+		},
+		setAccount(account) {
+			this.setState({account});
+			this.getValidation(account);
+		},
+		setBalance(event, value) {
+			this.setState({balance: parseFloat(value)});
+		},
+		setValidationDate(event, date) {
+			this.setState({validationDate: date});
+		},
+		getValidation(account) {
+			const validationStream = this.fetchLatestValidation(account)
+				.fetch().subscribe(
+					([validation]) => {
+						this.setState({lastValidation: validation});
+						const transactionsStream = this.fetchUnvalidatedTransactions(account, validation)
+							.watch().subscribe(
+								transactions => this.setState({transactions}),
+								error => console.error('[Failure]', 'Fetch transactions', error)
+							);
+						this.setStream('transactions', transactionsStream);
+					},
+					error => console.error('[Failure]', 'Fetch validation', error)
+				);
+			this.setStream('validation', validationStream);
+		},
+		validate() {
+			const validation = {
+				account: this.state.account,
+				balance: this.state.balance,
+				validatedAt: Date.now(),
+				validationDate: this.state.validationDate.getTime()
+			};
+			this.getValidationFeed().store(validation).subscribe(
+				() => {
+					console.log('Account balanced validated');
+					this.getValidation(this.state.account);
+				},
+				error => console.error('[Failure] Account validation', error)
+			);
+		},
+		deleteLastValidation() {
+			if (this.state.lastValidation) {
+				this.getValidationFeed().remove(this.state.lastValidation);
+				this.getValidation(this.state.account);
 			}
+		},
+		renderLastValidation() {
+			if (this.state.lastValidation) {
+				return <div className="last-validation">
+					<div style={{float: 'right'}}>
+						<FloatingActionButton primary={true} mini={true}
+							onTouchTap={this.cbks.deleteLastValidation}>
+								<DeleteIcon />
+						</FloatingActionButton>
+					</div>
+					Validé le {new Date(this.state.lastValidation.validatedAt).toLocaleString()} à hauteur de {this.state.lastValidation.balance.toFixed(2)} € le {new Date(this.state.lastValidation.validationDate).toLocaleDateString()}
+				</div>;
+			}
+		},
+		renderTransactions() {
+			if (!_.isEmpty(this.state.transactions)) {
+				// Group transactions by date
+				const lastWeek = new Date();
+				lastWeek.setDate(TODAY.getDate() - 7);
+				lastWeek.setHours(0, 0, 0, 0);
 
+				const groups = _(this.state.transactions)
+					.filter(transaction => transaction.date >= lastWeek)
+					.groupBy(transaction => new Date(transaction.date).setHours(0, 0, 0, 0))
+					.value();
+				const order = _(groups).keys()
+					.filter(key => !_.isEmpty(groups[key]))
+					.map(key => parseInt(key, 10))
+					.sort()
+					.reverse()
+					.map(key => ({
+						date: key,
+						caption: new Date(key).toLocaleDateString(),
+						transactions: groups[key]
+					}))
+					.value();
+				const oldTransactions = _.filter(this.state.transactions, transaction => transaction.date < lastWeek);
+				if (!_.isEmpty(oldTransactions)) {
+					order.push({
+						caption: 'Before',
+						transactions: oldTransactions
+					});
+				}
+
+				return <div>
+					{_.map(order, entry => {
+						return <div key={entry.date}>
+							<div style={{fontWeight: 'bold'}}>
+								{entry.caption}
+								{entry.date ? <AccountBalance account={this.state.account} date={entry.date} /> : null}
+							</div>
+							<TransactionsView transactions={entry.transactions} />
+						</div>;
+					})}
+				</div>;
+			}
+		},
+		render() {
 			return <div>
-				{_.map(groups, (transactions, date) => {
-					return <div key={date}>
-						<div style={{fontWeight: 'bold'}}>
-							{date}
-							{date !== 'Before' ? <AccountBalance account={this.state.account} date={date} /> : null}
-						</div>
-						<TransactionsView transactions={transactions} />
-					</div>;
-				})}
+				<div>
+					Solde du compte <AccountPicker value={this.state.account}
+						onSelect={this.cbks.setAccount} />
+					à hauteur de <TextField type="number" hintText="Solde du compte"
+						value={this.state.balance}
+						onChange={this.cbks.setBalance} />
+					au <DatePicker hintText="Date"
+								value={this.state.validationDate}
+								maxDate={TODAY}
+								onChange={this.cbks.setValidationDate}
+								autoOk={true} style={{display: 'inline-block'}}/>
+					<RaisedButton label="Valider" primary={true}
+						onTouchTap={this.cbks.validate}/>
+				</div>
+				<div>
+					{this.state.account ? <AccountBalance account={this.state.account}/> : null}
+					{this.renderLastValidation()}
+					{this.renderTransactions()}
+				</div>
 			</div>;
 		}
-	}
-
-	render() {
-		return <div>
-			<div>
-				Solde du compte <AccountPicker value={this.state.account}
-					onSelect={this.cbks.setAccount} />
-				à hauteur de <TextField type="number" hintText="Solde du compte"
-					value={this.state.balance}
-					onChange={this.cbks.setBalance} />
-				au <DatePicker hintText="Date"
-							value={this.state.validationDate}
-							maxDate={TODAY}
-							onChange={this.cbks.setValidationDate}
-							autoOk={true} style={{display: 'inline-block'}}/>
-				<RaisedButton label="Valider" primary={true}
-					onTouchTap={this.cbks.validate}/>
-			</div>
-			<div>
-				{this.state.account ? <AccountBalance account={this.state.account}/> : null}
-				{this.renderTransactions()}
-			</div>
-		</div>;
-	}
-
-}
-
-AccountValidator.contextTypes = {
-	horizons: React.PropTypes.shape({
-		validations: React.PropTypes.object,
-		transactions: React.PropTypes.object
-	})
-};
+	});
 
 export default AccountValidator;
